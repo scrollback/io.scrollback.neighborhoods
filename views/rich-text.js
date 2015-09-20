9210a23368aac73da891c150a5cf57f70f2102dc
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-09-20 22:24:29 +0530

    Use an array instead of creating new Text elements

--- views/rich-text.js
+++ views/rich-text.js
@@ -18,47 +18,46 @@ export default class RichText extends React.Component {
         return (
             <Text {...this.props} style={[ styles.text, this.props.style ]}>
                 {this.props.text.split("\n").map((text, index, arr) => {
-                    return (
-                        <Text key={"outer-" + index}>
-                            {text.split(" ").map((t, i) => {
-                                let items = [];
+                    return ([
+                        text.split(" ").map((t, i) => {
+                            let items = [];
 
-                                const key = "inner-" + index + "-" + i;
+                            const key = "inner-" + index + "-" + i;
 
-                                // Strip out ending punctuations
-                                let punctuation = "";
+                            // Strip out ending punctuations
+                            let punctuation = "";
 
-                                if (/[\.,\?!:;]$/.test(t)) {
-                                    punctuation = t.substring(t.length - 1);
+                            if (/[\.,\?!:;]$/.test(t)) {
+                                punctuation = t.substring(t.length - 1);
 
-                                    t = t.replace(/.$/, "");
-                                }
+                                t = t.replace(/.$/, "");
+                            }
 
-                                if (/^@[a-z0-9\-]+$/.test(t)) {
-                                    // a mention
-                                    items.push(<Link key={key}>{t}</Link>);
-                                } else if (/^#\S+$/.test(t)) {
-                                    // a hashtag
-                                    items.push(<Link key={key}>{t}</Link>);
-                                } else if (/^(http|https):\/\/(\S+)$/i.test(t)) {
-                                    // a link
-                                    items.push(<Link key={key} href={t}>{t}</Link>);
-                                } else if (/^[^@]+@[^@]+\.[^@]+$/i.test(t)) {
-                                    // an email id
-                                    items.push(<Link key={key} href={"mailto:" + t}>{t}</Link>);
-                                } else if (/^(?:\+?(\d{1,3}))?[-.\s(]*(\d{3})?[-.\s)]*(\d{3})[-.\s]*(\d{4})(?: *x(\d+))?$/.test(t)) {
-                                    // a phone number
-                                    items.push(<Link key={key} href={"tel:" + t}>{t}</Link>);
-                                } else {
-                                    return t + punctuation + " ";
-                                }
+                            if (/^@[a-z0-9\-]+$/.test(t)) {
+                                // a mention
+                                items.push(<Link key={key}>{t}</Link>);
+                            } else if (/^#\S+$/.test(t)) {
+                                // a hashtag
+                                items.push(<Link key={key}>{t}</Link>);
+                            } else if (/^(http|https):\/\/(\S+)$/i.test(t)) {
+                                // a link
+                                items.push(<Link key={key} href={t}>{t}</Link>);
+                            } else if (/^[^@]+@[^@]+\.[^@]+$/i.test(t)) {
+                                // an email id
+                                items.push(<Link key={key} href={"mailto:" + t}>{t}</Link>);
+                            } else if (/^(?:\+?(\d{1,3}))?[-.\s(]*(\d{3})?[-.\s)]*(\d{3})[-.\s]*(\d{4})(?: *x(\d+))?$/.test(t)) {
+                                // a phone number
+                                items.push(<Link key={key} href={"tel:" + t}>{t}</Link>);
+                            } else {
+                                return t + punctuation + " ";
+                            }
 
-                                items.push(punctuation + " ");
+                            items.push(punctuation + " ");
 
-                                return items;
-                            })}{index !== (arr.length - 1) ? "\n" : ""}
-                        </Text>
-                    );
+                            return items;
+                        }),
+                        index !== (arr.length - 1) ? "\n" : ""
+                    ]);
                 })}
             </Text>
         );
