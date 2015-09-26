24f7aea6d93d2e878867512779a345267bd74b47
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-09-26 18:18:58 +0530

    Improve no results page

--- views/page-empty.js
+++ views/page-empty.js
@@ -2,14 +2,23 @@ import React from "react-native";
 import Page from "./page";
 
 const {
+    StyleSheet,
     Text
 } = React;
+
+const styles = StyleSheet.create({
+    label: {
+        fontSize: 18,
+        paddingHorizontal: 8,
+        marginHorizontal: 8
+    }
+});
 
 export default class PageLoading extends React.Component {
     render() {
         return (
             <Page {...this.props}>
-                <Text>No results found.</Text>
+                <Text style={styles.label}>No results found.</Text>
             </Page>
         );
     }
