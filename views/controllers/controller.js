a3e3f2029b77c1e5afdb00fe4f1d1b9b8efa24dd
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-10-11 03:21:40 +0530

    Add query and dispatch methods to controller

--- views/controllers/controller.js
+++ views/controllers/controller.js
@@ -4,13 +4,8 @@ import store from "../../store/store";
 // Decorator for controllers
 export default function(Target) {
 	return class extends Target {
-		constructor(props) {
-			super(props);
-
-			this.core = core;
-			this.store = store;
-
-			this._handlers = [];
+		get store() {
+			return store;
 		}
 
 		componentDidMount(...args) {
@@ -34,21 +29,71 @@ export default function(Target) {
 			}
 		}
 
-		handle(event, cb, ...rest) {
-			if (typeof super.handle === "function") {
-				super.handle(event, cb, ...rest);
-			}
-
+		handle(event, cb, prio = 100) {
 			const handler = cb.bind(this);
 
 			// Attach the event handler
-			core.on(event, handler, ...rest);
+			core.on(event, handler, prio);
 
 			// Keep track so we can clean up later
-			this._handlers.push([ event, handler, ...rest ]);
+			this._handlers = this._handlers || [];
+			this._handlers.push([ event, handler, prio ]);
 
 			// Return the index
 			return this._handlers.length - 1;
+		}
+
+		query(type, params = {}) {
+			return new Promise((resolve, reject) => {
+				core.emit(type, params, (err, res) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(res);
+					}
+				});
+			});
+		}
+
+		dispatch(name, params = {}, prio = 1) {
+			return new Promise((resolve, reject) => {
+				const down = name + "-dn";
+
+				let id;
+
+				function cleanUp() {
+
+					/* eslint-disable no-use-before-define */
+					core.off(down, onSuccess);
+					core.off("error-dn", onError);
+				}
+
+				function onSuccess(action) {
+					if (id === action.id) {
+						resolve(action);
+						cleanUp();
+					}
+				}
+
+				function onError(error) {
+					if (id === error.id) {
+						reject(error);
+						cleanUp();
+					}
+				}
+
+				core.on(down, onSuccess, prio);
+				core.on("error-dn", onError, prio);
+
+				core.emit(name + "-up", params, (err, action) => {
+					id = action.id;
+
+					if (err) {
+						reject(err);
+						cleanUp();
+					}
+				});
+			});
 		}
 	};
 }
