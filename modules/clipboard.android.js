fb441819a0a0f79c968af2919f1a4f8e977b9a15
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-10-17 23:16:40 +0530

    Fix toast text

--- modules/clipboard.android.js
+++ modules/clipboard.android.js
@@ -5,7 +5,7 @@ const { ClipboardModule } = NativeModules;
 export default {
 	setText(text) {
 		ClipboardModule.setText("Copied text from Hey, Neighbor!", text, () => {
-			ToastAndroid.show("Copied to the Clipboard", ToastAndroid.SHORT);
+			ToastAndroid.show("Copied to clipboard", ToastAndroid.SHORT);
 		});
 	},
 
