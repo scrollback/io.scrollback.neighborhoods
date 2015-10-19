cd419581ddde4c6e8b2495e46facc46b063aee95
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-10-19 15:56:59 +0530

    Fix pushnotification unregister arguments

--- push-notification/gcm.js
+++ push-notification/gcm.js
@@ -44,6 +44,8 @@ function initialize() {
 							core.emit("user-up", {
 								to: user.id,
 								user
+							}, () => {
+								AsyncStorage.setItem(KEY_REGISTER_TIME, Date.now()).catch(() => {});
 							});
 						});
 					}
@@ -52,7 +54,7 @@ function initialize() {
 	});
 
 	core.on("logout", () => {
-		PushNotification.unRegisterGCM();
+		PushNotification.unRegisterGCM(() => {});
 		AsyncStorage.removeItem(KEY_REGISTER_TIME).catch(() => {});
 	});
 }
