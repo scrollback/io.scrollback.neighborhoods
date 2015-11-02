e3fd3262faec7c91bd390b3cc0a9e36f14573ef6
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-11-02 18:23:56 +0530

    Fix indentation

--- index.android.js
+++ index.android.js
@@ -2,13 +2,13 @@ import React from "react-native";
 import AppController from "./views/controllers/app-controller";
 
 const {
-    AppRegistry
+	AppRegistry
 } = React;
 
 export default class HeyNeighbor extends React.Component {
-    render() {
-        return <AppController />;
-    }
+	render() {
+		return <AppController />;
+	}
 }
 
 AppRegistry.registerComponent("HeyNeighbor", () => HeyNeighbor);
