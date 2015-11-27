da95d35efc81aa1c6412561563d4fb72a5fffc71
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-11-27 12:10:38 +0530

    Add a fancy loading component

--- views/components/loading-item.js
+++ views/components/loading-item.js
@@ -1,10 +1,8 @@
 import React from "react-native";
-import Colors from "../../colors.json";
-import Loading from "./loading";
+import LoadingFancy from "./loading-fancy";
 
 const {
 	StyleSheet,
-	PixelRatio,
 	View
 } = React;
 
@@ -12,36 +10,14 @@ const styles = StyleSheet.create({
 	container: {
 		alignSelf: "stretch",
 		alignItems: "center"
-	},
-	loadingContainer: {
-		backgroundColor: Colors.white,
-		borderColor: Colors.separator,
-		borderWidth: 1 / PixelRatio.get(),
-		height: 36,
-		width: 36,
-		borderRadius: 18,
-		alignItems: "center",
-		justifyContent: "center",
-		margin: 24,
-		elevation: 1
-	},
-	loading: {
-		height: 24,
-		width: 24
 	}
 });
 
 export default class LoadingItem extends React.Component {
-	shouldComponentUpdate() {
-		return false;
-	}
-
 	render() {
 		return (
-			<View style={styles.container}>
-				<View style={styles.loadingContainer}>
-					<Loading style={styles.loading} />
-				</View>
+			<View style={[ styles.container, this.props.style ]}>
+				<LoadingFancy />
 			</View>
 		);
 	}
