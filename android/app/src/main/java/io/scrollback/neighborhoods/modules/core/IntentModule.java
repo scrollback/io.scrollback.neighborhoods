ec82c991b6e70d0ec6c53260f117e7bf20662ef9
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-12-08 19:36:37 +0530

    Fix intent module

--- android/app/src/main/java/io/scrollback/neighborhoods/modules/core/IntentModule.java
+++ android/app/src/main/java/io/scrollback/neighborhoods/modules/core/IntentModule.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.JSApplicationIllegalArgumentException;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
